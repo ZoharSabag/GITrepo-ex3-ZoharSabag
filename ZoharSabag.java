13d5bb77c97a719e1f1e138854e49521d23b0e37
ZoharSabag <zoharsabag2@gmail.com>
2024-08-17 17:03:42 +0300

    change print in zoharSabag file

--- ZoharSabag.java
+++ ZoharSabag.java
@@ -1,7 +1,7 @@
 public class ZoharSabag{
 	public static void main(String[] args){
 		for(int i=1;i<=10;i++){
-			System.out.println("Zohar Sabag "+i);
+			System.out.println("My name is: Zohar Sabag "+i);
 			
 			}
 	}
